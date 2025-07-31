474dcd8a34fd6c10adf356721e01dc083579a770
Viktoria-pas <vpasechnyuk@yandex.com>
2025-07-31 17:07:22 +0200

    Разделила классы исключений

--- gateway/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ItemController.class)
-class ItemControllerCompleteTest {
+class ItemControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
